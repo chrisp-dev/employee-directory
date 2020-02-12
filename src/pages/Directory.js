ef46f9f3775b01ff5d942024162add184a22fbc5
chrisp-dev <chris.paradeise@gmail.com>
2020-02-11 18:45:55 -0800

    Created directory page

---
+++ src/pages/Directory.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import API from '../utils/API';
+import Wrapper from '../components/Wrapper';
+import Container from '../components/Container';
+import Row from '../components/Row';
+import SearchForm from '../components/SearchForm';
+import SearchResults from '../components/SearchResults';
+
+function Directory() {
+    const [users, setUsers] = useState([]);
+    const [userSort, setUserSort] = useState({});
+    const [search, setSearch] = useState("");
+
+    useEffect(() => {
+        API.getUsers().then(usersData => {
+            const userData = usersData.results;
+            setUsers(userData);
+        });
+    }, []);
+
+    const handleInputChange = event => {
+        setSearch({ ...search, search: event.target.value });
+    };
+    return (
+        <Wrapper>
+            <h1 className="jumbotron text-center">Search User Directory</h1>
+            <SearchForm search={search} handleInputChange={handleInputChange} />
+
+            <SearchResults users={users}
+                title="Employee Directory Results"
+                description="Results"
+                url=""
+            />
+        </Wrapper>
+    )
+}
+
+export default Directory;
